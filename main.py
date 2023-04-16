a13717f38dc4b25b2c9d5d16578ee5d7390b6ccd
Hamchin <k.terada.1211@gmail.com>
2023-04-16 15:04:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,51 +6,68 @@ import argparse
 import os
 import subprocess
 
-from data import DATA_LIST
+from data_dict import DATA_DICT
+from data_list import DATA_LIST
 
 COOKIE = os.getenv("COOKIE", "")
 HOME_DIR = os.getenv("HOME", "")
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", f"{HOME_DIR}/Downloads/data")
+OUTPUT_BASE_DIR = os.getenv("OUTPUT_BASE_DIR", f"{HOME_DIR}/Downloads/data")
 START = int(os.getenv("START", 1))
 
-def get_command(mode: str) -> str:
-    if mode == "video":
+
+def get_command(output_type: str) -> str:
+    if output_type == "video":
         if COOKIE:
             return 'ffmpeg -headers "Cookie: %s" -i "{{URL}}" -c copy "{{OUTPUT_PATH}}"' % COOKIE
         else:
             return 'ffmpeg -i "{{URL}}" -c copy "{{OUTPUT_PATH}}"'
-    elif mode == "image":
+    elif output_type == "image":
         if COOKIE:
             return 'wget --header "cookie: %s" "{{URL}}" -O "{{OUTPUT_PATH}}"' % COOKIE
         else:
             return 'wget "{{URL}}" -O "{{OUTPUT_PATH}}"'
     else:
-        raise ValueError(f"INVALID_MODE: {mode}")
+        raise ValueError(f"INVALID_OUTPUT_TYPE: {output_type}")
 
 
-def download(mode: str, data_type: str, command: str) -> None:
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    if data_type == "simple":
-        for i, url in enumerate(DATA_LIST):
-            mode_to_ext = {"video": "mp4", "image": "png"}
-            output_path = os.path.join(OUTPUT_DIR, f"{START+i:04}.{mode_to_ext[mode]}")
+def download(data_list: list, input_type: str, output_type: str, command: str, output_dir: str = OUTPUT_BASE_DIR) -> None:
+    os.makedirs(output_dir, exist_ok=True)
+    if input_type == "simple":
+        for i, url in enumerate(data_list):
+            type_to_ext = {"video": "mp4", "image": "png"}
+            output_path = os.path.join(output_dir, f"{START+i:04}.{type_to_ext[output_type]}")
             subprocess.run(command.replace("{{URL}}", url).replace("{{OUTPUT_PATH}}", output_path), shell=True, check=True)
-    elif data_type == "standard":
-        for url, output_filename in DATA_LIST:
-            output_path = os.path.join(OUTPUT_DIR, output_filename)
+    elif input_type == "standard":
+        for url, output_filename in data_list:
+            output_path = os.path.join(output_dir, output_filename)
             subprocess.run(command.replace("{{URL}}", url).replace("{{OUTPUT_PATH}}", output_path), shell=True, check=True)
+    else:
+        raise ValueError(f"INVALID_INPUT_TYPE: {input_type}")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-type", choices=["list", "dict"], required=True)
+    parser.add_argument("--input-type", choices=["simple", "standard"], required=True)
+    parser.add_argument("--output-type", choices=["video", "image"], required=True)
+
+    args = parser.parse_args()
+    data_type = args.data_type
+    input_type = args.input_type
+    output_type = args.output_type
+
+    command = get_command(output_type)
+
+    if data_type == "list":
+        data_list = DATA_LIST
+        download(data_list, input_type, output_type, command)
+    elif data_type == "dict":
+        for dir_name, data_list in DATA_DICT.items():
+            output_dir = os.path.join(OUTPUT_BASE_DIR, dir_name)
+            download(data_list, input_type, output_type, command, output_dir)
     else:
         raise ValueError(f"INVALID_DATA_TYPE: {data_type}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", choices=["video", "image"], required=True)
-    parser.add_argument("--data-type", choices=["simple", "standard"], required=True)
-
-    args = parser.parse_args()
-    mode = args.mode
-    data_type = args.data_type
-
-    command = get_command(mode)
-    download(mode, data_type, command)
+    main()
