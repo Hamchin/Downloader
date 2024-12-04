fbe3aef1ac0e4b6906b2d097329245117ad39113
Kenta Terada <k.terada.1211@gmail.com>
2024-12-05 00:00:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import json
 import os
 import subprocess
 from enum import Enum
+from urllib.parse import urlparse
+
+from yt_dlp import YoutubeDL
+
 
 COOKIE = os.getenv("COOKIE", "")
 HOME_DIR = os.getenv("HOME", "")
@@ -16,14 +20,24 @@ START = int(os.getenv("START", 1))
 
 
 class DataType(Enum):
+    """
+    データの種類.
+    """
+
     IMAGE = 1
     VIDEO = 2
+    YOUTUBE = 3
 
 
 def get_data_type(url: str) -> DataType:
     """
     URL からデータの種類を取得する.
     """
+    parsed = urlparse(url)
+
+    if parsed.netloc == "www.youtube.com":
+        return DataType.YOUTUBE
+
     if url.endswith((".png", ".jpg", ".jpeg")):
         return DataType.IMAGE
 
@@ -35,7 +49,7 @@ def get_data_type(url: str) -> DataType:
 
 def get_command_for_image(url: str, output_path: str) -> str:
     """
-    画像用のコマンドを取得する.
+    画像をダウンロードするためのコマンドを取得する.
     """
     if COOKIE:
         return f'wget --header "cookie: {COOKIE}" "{url}" -O "{output_path}"'
@@ -45,7 +59,7 @@ def get_command_for_image(url: str, output_path: str) -> str:
 
 def get_command_for_video(url: str, output_path: str) -> str:
     """
-    動画用のコマンドを取得する.
+    動画をダウンロードするためのコマンドを取得する.
     """
     if COOKIE:
         return f'ffmpeg -headers "Cookie: {COOKIE}" -i "{url}" -c copy "{output_path}"'
@@ -69,6 +83,17 @@ def download_data(url: str, output_path: str) -> None:
         subprocess.run(command, shell=True, check=True)
         return
 
+    if data_type == DataType.YOUTUBE:
+        output_path_without_ext, ext = output_path.rsplit(os.extsep, 1)
+        params = {
+            "format": "bestvideo+bestaudio/best",
+            "outtmpl": output_path_without_ext,
+            "merge_output_format": ext,
+        }
+        with YoutubeDL(params) as ydl:
+            ydl.download([url])
+        return
+
 
 def download_data_list(data_list: list, input_format: str, output_dir: str = OUTPUT_BASE_DIR) -> None:
     """
@@ -79,7 +104,7 @@ def download_data_list(data_list: list, input_format: str, output_dir: str = OUT
     if input_format == "simple":
         for i, url in enumerate(data_list):
             data_type = get_data_type(url)
-            type_to_ext = {DataType.IMAGE: "png", DataType.VIDEO: "mp4"}
+            type_to_ext = {DataType.IMAGE: "png", DataType.VIDEO: "mp4", DataType.YOUTUBE: "mp4"}
             output_path = os.path.join(output_dir, f"{START+i:04}.{type_to_ext[data_type]}")
             download_data(url, output_path)
         return
